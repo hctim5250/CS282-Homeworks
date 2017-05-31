38cd6fc15c17c97c2d28cdbf2e133e06ad737343
hctim5250 <hctim5250@gmail.com>
2017-05-31 20:20:14 +0800

    增加第二講範例

---
+++ Lecture02-Examples/Example03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sql =
+                @"select * from Table
+                    where id > 10    
+                ";
+            Console.WriteLine(sql);
+
+            string path = @"C:\Users\user\Desktop\20170526\Lecture02-Examples";
+        }
+    }
+}
---
+++ Lecture02-Examples/Example05/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example05
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Person person = new Person()
+            {
+                Name = "Tim",
+                Birthday = "0522",
+                Email = "hctim5250@gmail.com",
+                Orientation = 0.0,
+                Pos = Position.Generate()
+            };
+
+            Console.Write("請輸入正方形邊長:");
+            double width = 0.0;
+            bool success = double.TryParse(Console.ReadLine(), out width);
+            if (success)
+                person.DrawSquare(width);
+            else
+                Console.WriteLine("輸入錯誤，請重新輸入!");
+        }
+    }
+}
