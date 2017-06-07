33eb90a23eb06654ba91f251561405891e191c5b
hctim5250 <hctim5250@gmail.com>
2017-06-07 10:58:29 +0800

    增加第四講範例

---
+++ Lecture04-Examples/Lecture04-Examples/Example01/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example01
+{
+    public partial class Form1 : Form
+    {
+        private Scene currentScene;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.currentScene = new Scene(this.scenePictureBox);
+        }
+
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+            this.MakeDecision(1);                                      //按下按鈕後，將這個Form的scene的picturebox變為圖片1
+        }
+
+        private void noButton_Click(object sender, EventArgs e)
+        {
+            this.MakeDecision(2);
+        }
+
+        private void MakeDecision(int decision)
+        {
+            switch (this.currentScene.Number)
+            {
+                case 1:
+                    this.currentScene.ChangeTo(2);
+                    break;
+
+                case 2:
+                    if (decision == 1)
+                        this.currentScene.ChangeTo(8);
+                    else
+                        this.currentScene.ChangeTo(3);
+                    break;
+            }
+        }
+    }
+}
---
+++ Lecture04-Examples/Lecture04-Examples/Example01/Scene.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example01
+{
+    public class Scene
+    {
+        public int Number;
+
+        public PictureBox TargetPictureBox;
+
+        public Scene(PictureBox target)                             //建構場景時:
+        {
+            this.Number = 1;
+            this.TargetPictureBox = target;
+            this.TargetPictureBox.Image = this.GetImage();          //這個的目標picturebox的image=這個getimage()
+        }
+
+        public Image GetImage()
+        {
+            if (this.Number == 1)
+                return global::Example01.Properties.Resources._1;   //C# Resource Image Array 簡化
+            else if (this.Number == 2)
+                return global::Example01.Properties.Resources._2;
+            else if (this.Number == 3)
+                return global::Example01.Properties.Resources._3;
+            else if (this.Number == 4)
+                return global::Example01.Properties.Resources._4;
+            else if (this.Number == 5)
+                return global::Example01.Properties.Resources._5;
+            else if (this.Number == 6)
+                return global::Example01.Properties.Resources._6;
+            else if (this.Number == 7)
+                return global::Example01.Properties.Resources._7;
+            else if (this.Number == 8)
+                return global::Example01.Properties.Resources._8;
+            else if (this.Number == 9)
+                return global::Example01.Properties.Resources._9;
+            else
+                return global::Example01.Properties.Resources._10;
+        }
+
+        public void ChangeTo(int target)
+        {
+            this.Number = target;
+            this.TargetPictureBox.Image = this.GetImage();
+        }
+    }
+}
