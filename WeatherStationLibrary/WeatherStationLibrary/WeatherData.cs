07ed07edccb428a5c1b74c014ca32d76402e3cab
hctim5250 <hctim5250@gmail.com>
2017-06-10 01:34:51 +0800

    Add Lecture05 Example 01 and 02

--- WeatherStationLibrary/WeatherStationLibrary/WeatherData.cs
+++ WeatherStationLibrary/WeatherStationLibrary/WeatherData.cs
@@ -8,9 +8,77 @@ namespace WeatherStationLibrary
 {
     public class WeatherData
     {
-        public double Temperature;
-        public double Humidity;
-        public double Pressure;
+        private double temperature;
+        private double humidity;
+        private double pressure;
+
+        public double Temperature
+        {
+            get
+            {
+                return this.temperature;
+            }
+
+            set
+            {
+                if (value > 40)
+                {
+                    this.temperature = 40;
+                }
+                else if (value < -10)
+                {
+                    this.temperature = -10;
+                }
+                else
+                {
+                    this.temperature = value;
+                }
+            }
+        }
+
+        public double Humidity
+        {
+            get
+            {
+                return this.humidity;
+            }
+
+            set
+            {
+                if (value > 100)
+                {
+                    this.humidity = 100;
+                }
+                else if (value < 0)
+                {
+                    this.humidity = 0;
+                }
+                else
+                    this.humidity = value;
+            }
+        }
+
+        public double Pressure
+        {
+            get
+            {
+                return this.pressure;
+            }
+
+            set
+            {
+                if (value > 1)
+                {
+                    this.pressure = 1;
+                }
+                else if (value < 0)
+                {
+                    this.pressure = 0;
+                }
+                else
+                    this.pressure = value;
+            }
+        }
 
         public bool Equals(WeatherData other)
         {
